6e5414b0be00abece6651c1d5ee9c769fdde76c4
Yury <yurydgfuentes@gmail.com>
2022-01-28 12:23:57 +0200

    allow get pictures from google

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'], // allow nextjs get image from google
+  },
 }
 
 module.exports = nextConfig
