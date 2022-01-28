a3d054de0c10f2675e94332143a0837ef43770d6
Yury <yurydgfuentes@gmail.com>
2022-01-28 12:26:26 +0200

    use context in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useAuth } from '../context'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const { login, logout, currentUser } = useAuth();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,44 +17,39 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Nextjs + Firebase 9
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <div className='shadow p-5 w-2/6'>
+          {
+            currentUser && (
+              <div className=' flex items-center justify-between'>
+                <h2>{currentUser?.displayName}</h2>
+                <Image
+                  alt='Avatar'
+                  className=' rounded'
+                  src={currentUser.photoURL as string}
+                  width={68}
+                  height={68}
+                />
+              </div>
+            )
+          }
+          <div>
+            {
+              !currentUser && <button className=' px-3 py-2 rounded bg-gray-100' onClick={login}>
+                Login
+              </button>
+            }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            {
+              currentUser && <button className=' px-3 py-2 rounded bg-gray-100' onClick={logout}>
+                Logout
+              </button>
+            }
+          </div>
         </div>
-      </main>
+      </main >
 
       <footer className={styles.footer}>
         <a
@@ -65,7 +63,7 @@ const Home: NextPage = () => {
           </span>
         </a>
       </footer>
-    </div>
+    </div >
   )
 }
 
